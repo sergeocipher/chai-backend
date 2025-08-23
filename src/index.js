6bde9ac39ebc7e3cd11346ba15c17b81c96d9ec3
Somyajit <somyajitsaha621@gmail.com>
2025-08-23 18:24:31 +0530

    error standardization

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import dotenv from  "dotenv"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({
     path: "./.env"
 })
 
 connectDB()
+.then(() =>{
+    const connection = app.listen(process.env.PORT || 3000 , () =>{
+        console.log(`server running at ${process.env.PORT}`)
+    })
+
+    connection.on("error" , (error) =>{
+        console.log("failure !!!!")
+    })
+}) 
+.catch((err) =>{
+    console.log("MONGO coneection  failed !!!!!!!!!!!!!!!!!!!!" , err)
+})
 
 
 
