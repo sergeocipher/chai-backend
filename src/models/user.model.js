291547d67f01009f0852178f76dde97ef2c5d878
Somyajit <somyajitsaha621@gmail.com>
2025-09-03 14:30:13 +0530

    comment

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,6 +52,7 @@ const userSchema = new Schema(
     }
 )
 
+// comparing password 
 userSchema.pre("save" , async function(next){
     if(!this.isModified("password")) return next();
     this.password = bcrypt.hash(this.password , 10);
