315e2da770060d2c93110962c463e46637fbe6fd
Somyajit <somyajitsaha621@gmail.com>
2025-09-07 09:26:59 +0530

    cloudinary-setup

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,25 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    const response = await cloudinary.uploader.upload(localFilePath, {resource_type: "auto"});
+    //upload the file succesfully 
+
+    console.log("File uploaded succesfully" , response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath)
+    //remove the locally failed temporary file as the opreation get failed 
+    return null;
+
+  }
+};
+
+export {uploadOnCloudinary};
